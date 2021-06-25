f7c3c6c489cdd8be774d9c44474bdbca51a410c1
Visty31 <85346723+Visty31@users.noreply.github.com>
2021-06-25 14:15:18 +0300

    Add files via upload

---
+++ VISTY 2.c
@@ -0,0 +1,18 @@
+// if else statement
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	
+    printf("Enter the values of A and B\n");
+	scanf("%d%d",&a,&b);
+	
+	if(a==b)
+		printf("THE VALUES ARE EQUAL");
+	else 
+	{
+		printf("THE VALUES ARE NOT EQUAL");	
+	}
+	
+   return 0;
+}
